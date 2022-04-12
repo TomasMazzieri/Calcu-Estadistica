1a572c191df8ebd2ae2e1d418727d2a5979fd23a
Tomás <66665162+TomasMazzieri@users.noreply.github.com>
2022-04-11 22:41:12 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace matematica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.btnModa = new System.Windows.Forms.Button();
+            this.btnMediana = new System.Windows.Forms.Button();
+            this.lblMediana = new System.Windows.Forms.Label();
+            this.lblModa = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Location = new System.Drawing.Point(138, 92);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(75, 23);
+            this.btnMedia.TabIndex = 0;
+            this.btnMedia.Text = "Media";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 20);
+            this.label1.TabIndex = 1;
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.Location = new System.Drawing.Point(150, 69);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(49, 20);
+            this.lblMedia.TabIndex = 2;
+            // 
+            // btnModa
+            // 
+            this.btnModa.Location = new System.Drawing.Point(31, 92);
+            this.btnModa.Name = "btnModa";
+            this.btnModa.Size = new System.Drawing.Size(75, 23);
+            this.btnModa.TabIndex = 3;
+            this.btnModa.Text = "Moda";
+            this.btnModa.UseVisualStyleBackColor = true;
+            this.btnModa.Click += new System.EventHandler(this.btnModa_Click);
+            // 
+            // btnMediana
+            // 
+            this.btnMediana.Location = new System.Drawing.Point(246, 92);
+            this.btnMediana.Name = "btnMediana";
+            this.btnMediana.Size = new System.Drawing.Size(75, 23);
+            this.btnMediana.TabIndex = 4;
+            this.btnMediana.Text = "Mediana";
+            this.btnMediana.UseVisualStyleBackColor = true;
+            this.btnMediana.Click += new System.EventHandler(this.btnMediana_Click);
+            // 
+            // lblMediana
+            // 
+            this.lblMediana.Location = new System.Drawing.Point(257, 68);
+            this.lblMediana.Name = "lblMediana";
+            this.lblMediana.Size = new System.Drawing.Size(49, 20);
+            this.lblMediana.TabIndex = 5;
+            // 
+            // lblModa
+            // 
+            this.lblModa.Location = new System.Drawing.Point(43, 68);
+            this.lblModa.Name = "lblModa";
+            this.lblModa.Size = new System.Drawing.Size(49, 20);
+            this.lblModa.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 178);
+            this.Controls.Add(this.lblModa);
+            this.Controls.Add(this.lblMediana);
+            this.Controls.Add(this.btnMediana);
+            this.Controls.Add(this.btnModa);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMedia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Button btnModa;
+        private System.Windows.Forms.Button btnMediana;
+        private System.Windows.Forms.Label lblMediana;
+        private System.Windows.Forms.Label lblModa;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matematica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //variables
+        int[] Datos = new int[9];
+
+        int[] DatosOrdenados;
+
+        int[] xi;
+
+        /*int[] FrecuenciaAbsoluta;*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //instanciando clase predefinida en visual, que me permite generar en un determinado intervalo
+            Random random = new Random();
+
+
+            //Defino una variable para la cantidad de digitos
+            int limite = 9;
+
+            Datos[0] = random.Next(limite);
+            Datos[1] = random.Next(limite);
+            Datos[2] = random.Next(limite);
+            Datos[3] = random.Next(limite);
+            Datos[4] = random.Next(limite);
+            Datos[5] = random.Next(limite);
+            Datos[6] = random.Next(limite);
+            Datos[7] = random.Next(limite);
+            Datos[8] = random.Next(limite);
+
+
+            //el array hereda las propiedad de Datos
+            DatosOrdenados = Datos;
+            OrdenarDatos();
+            MostrarLabel();
+            xicalc();
+
+        }
+        //Definir for para mostrar los datos en label
+        private void MostrarLabel()
+        {
+            for (int i = 0; i < Datos.Length; i++)
+            {
+                label1.Text = label1.Text + DatosOrdenados[i] + ", ";
+            }
+        }
+        #region Parametros Centralización
+        //Defino funcion para ordenar los datos a menor a mayor
+        private void OrdenarDatos()
+        {
+            //funcion para ordenar los datos random 
+            Array.Sort(DatosOrdenados);
+        }
+
+        private void xicalc()
+        {
+
+            int Contador = 1;
+            int DatoActual = DatosOrdenados[0];
+
+            for (int i = 0; i < DatosOrdenados.Length; i++)
+            {
+                if (DatoActual != DatosOrdenados[i])
+                {
+                    DatoActual = DatosOrdenados[i];
+                    Contador = Contador + 1;
+                }
+            }
+
+            xi = new int[Contador]; 
+            DatoActual = DatosOrdenados[0];
+            int Posicion = 0;
+
+            for (int i = 0; i < DatosOrdenados.Length; i++)
+            {
+                if (DatoActual != DatosOrdenados[i])
+                {
+                    xi[Posicion] = DatoActual;
+                    DatoActual = DatosOrdenados[i];
+                    Posicion = Posicion + 1;
+                }
+            }
+            xi[xi.Length - 1] = DatosOrdenados[DatosOrdenados.Length - 1];
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            //media
+            //le doy a resultado, el valor 0 para comenzar la suma del primer dato
+            double Resultado = 0;
+
+            for (int i = 0; i < Datos.Length; i++)
+            {
+                Resultado = Resultado + Datos[i];
+            }
+
+            Resultado = Resultado / Datos.Length;
+
+            lblMedia.Text = Convert.ToString(Resultado);
+        }
+
+
+
+        private void btnModa_Click(object sender, EventArgs e)
+        {
+            int[] FrecuenciaAbsoluta = new int[0];  
+            int DatoActual = DatosOrdenados[0];
+            int Contador = 1;
+
+            for (int i = 0; i < DatosOrdenados.Length; i++)
+            {
+                if (DatoActual == DatosOrdenados[i])
+                {
+                    DatoActual = DatosOrdenados[i];
+                    Contador = Contador + 1;
+                }
+            }
+
+            FrecuenciaAbsoluta = new int[Contador];
+            DatoActual = DatosOrdenados[0];
+            Contador = 0;
+            int Posicion = 0;
+
+            for (int i = 0; i < DatosOrdenados.Length; i++)
+            {
+                if (DatoActual != DatosOrdenados[i])
+                {
+                    DatoActual = DatosOrdenados[i];
+                    FrecuenciaAbsoluta[Posicion] = Contador;
+                    Posicion = Posicion + 1;
+                    Contador = 0;
+                }
+
+                else
+                {
+                    Contador = Contador + 1;
+                }
+            }
+
+            DatoActual = FrecuenciaAbsoluta[0];
+            int Resultado = 0;
+
+            for (int i = 0; i < FrecuenciaAbsoluta.Length; i++)
+            {
+                for (int j = 0; j < FrecuenciaAbsoluta.Length; j++)
+                {
+                    if (FrecuenciaAbsoluta[i] > FrecuenciaAbsoluta[j])
+                    {
+                        Resultado = i;
+                        DatoActual = FrecuenciaAbsoluta[i];
+                    }
+                }
+            }
+
+            lblModa.Text = Convert.ToString(Resultado);
+        }
+
+
+        private void btnMediana_Click(object sender, EventArgs e)
+        {
+            int mediana = 0;
+
+            if ((DatosOrdenados.Length % 2) == 0)
+            {
+                mediana = DatosOrdenados.Length / 2;
+                lblMediana.Text = Convert.ToString((DatosOrdenados[mediana] + DatosOrdenados[mediana - 1]) / (double) 2 );
+            }
+            else
+            {
+                mediana = DatosOrdenados.Length / 2;
+                lblMediana.Text = Convert.ToString(DatosOrdenados[mediana]);
+            }
+        }
+        #endregion
+
+    }
+}
